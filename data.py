997eb9702e438e57a6782d28262c2fc6b4db4b7a
Nikolay Petrov <nikp@OSX-NIKP-N-BG.local>
2025-09-19 22:40:14 +0300

    Transfering all the data to the mock api

---
+++ data.py
@@ -0,0 +1,38 @@
+
+mock_profile_data = [
+    [100, 200, 300, 400],
+    [50, 80, 120, 160],
+    [50, 120, 180, 240],
+    [50, 120, 180, 240],
+  ]
+
+mock_transactions = [
+    [100, 200, 300, 400],
+    [50, 80, 120, 160],
+    [50, 120, 180, 240],
+    [50, 120, 180, 240],
+    [50, 120, 180, 240],
+  ],
+
+mock_profile_config = {
+    "headers": [
+        "",
+        "Total Referred",
+        "Total Active",
+        "Paid Out",
+        "Total Unclaimed",
+    ],
+    "rowLabels": ["Direct", "Level 2", "Level 3", "Total"],
+}
+
+
+mock_transactions_config = {
+    "headers": [
+        "Date",
+        "Total Referred",
+        "Total Active",
+        "Paid Out",
+        "Total Unclaimed",
+    ],
+    "rowLabels": ["Direct", "Level 2", "Level 3", "Level 4", "Total"],
+}
--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from fastapi import FastAPI
-from typing import Dict
-
+from fastapi import FastAPI,HTTPException
+import data
 app = FastAPI()
 
-# Mock data
-mock_profile_data = [
-    [100, 200, 300, 400],
-    [50, 80, 120, 160],
-    [50, 120, 180, 240],
-    [50, 120, 180, 240],
-  ]
 
-# Simple GET route
+
 @app.get("/profile")
 async def get_profile():
-    return mock_profile_data
+    return data.mock_profile_data
+
+@app.get("/config")
+async def get_config(type: str ):
+    if type == "profile":
+        return data.mock_profile_config
+    elif type == "transactions":
+        return data.mock_transactions_config
+    else:
+        raise HTTPException(status_code=404, detail=f"Config type '{type}' not found")
+
+@app.get("/transactions")
+async def get_transcations():
+    return data.mock_transactions
+
